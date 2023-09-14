393255d7e4ab8b0bd15fd58920c7ae9c2c5384b1
Ezra <ezraermy@gmail.com>
2023-09-14 21:49:59 +0300

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,7 @@
 require_relative 'application'
 
 # Initialize the Rails application.
+env_vars = File.join(Rails.root, 'config', 'env_variables.rb')
+load(env_vars) if File.exist?(env_vars)
 Rails.application.initialize!
+
